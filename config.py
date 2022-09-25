a3513efa27d6b79f2dd31062fffeccb785a2e874
yzung0123 <113013012+yzung0123@users.noreply.github.com>
2022-09-25 20:51:22 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "database-1.chdjbxjlh0mf.us-east-1.rds.amazonaws.com"
+customhost = "database-2.chdjbxjlh0mf.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "sample"
